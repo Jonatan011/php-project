429466dc85b6b0ce2b9b73bc8bff2c18cfbe2173
Jonatan <jonatan.munoz@mawiapps.com.co>
2024-10-07 16:18:58 -0500

    correccion

--- .vercel/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Instalar las dependencias de Composer
-composer install --optimize-autoloader --no-dev
-
-# Ejecutar las migraciones de la base de datos (opcional)
-php artisan migrate --force

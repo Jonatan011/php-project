654ae7764bd6236e42548713423486155d553b66
Jonatan <jonatan.munoz@mawiapps.com.co>
2024-10-07 17:32:19 -0500

    correccion

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,30 +7,6 @@
 </head>
 <body>
     <h1>Posts Disponibles</h1>
-
-    <ul>
-        <!-- Verifica si hay posts en la colección -->
-        @if ($posts->count() > 0)
-            @foreach ($posts as $post)
-                <li>
-                    <!-- Mostrar título y contenido del post -->
-                    <h2>{{ $post->title }}</h2>
-                    <p>{{ $post->body }}</p>
-
-                    <!-- Botón de editar -->
-                    <a href="/posts/{{ $post->id }}/edit">Editar</a>
-
-                    <!-- Formulario para eliminar el post -->
-                    <form action="/posts/{{ $post->id }}" method="POST" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit">Eliminar</button>
-                    </form>
-                </li>
-            @endforeach
-        @else
-            <li>No hay posts disponibles.</li>
-        @endif
     </ul>
 </body>
 </html>

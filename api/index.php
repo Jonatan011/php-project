79880de4f22f7092dbcb295f6d4420029c368c9c
Jonatan <jonatan.munoz@mawiapps.com.co>
2024-10-07 16:22:08 -0500

    correccion

--- api/index.php
+++ api/index.php
@@ -32,7 +32,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 */
 
 require __DIR__.'/../vendor/autoload.php';
-require __DIR__ . '/../public/index.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -46,7 +45,6 @@ require __DIR__ . '/../public/index.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
 
 $kernel = $app->make(Kernel::class);
 

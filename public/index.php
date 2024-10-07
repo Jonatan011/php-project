4c326cd49643c5f5ac93e6cd7507e8d7349e3e5a
Jonatan <jonatan.munoz@mawiapps.com.co>
2024-10-07 17:06:25 -0500

    correccion

--- public/index.php
+++ public/index.php
@@ -11,6 +11,10 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
